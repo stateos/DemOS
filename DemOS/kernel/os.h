1ced1e2f0eeba3a28562b0bf58035620924c55fa
Rajmund Szymanski <rajszym@o2.pl>
2018-10-14 20:19:36 +0200

    updated signals

--- DemOS/kernel/os.h
+++ DemOS/kernel/os.h
@@ -275,10 +275,11 @@ typedef uint_fast8_t sem_t;
 // definition of protected signal
 typedef unsigned sig_t;
 
-#define UINT_BIT      (sizeof(unsigned) * CHAR_BIT)
+#define SIG_LIMIT     (sizeof(unsigned) * CHAR_BIT)
 
-#define SIGSET(signo) (((signo) < UINT_BIT) ? 1U << (signo) : 0U)   // signal mask from the given signal number
-#define sigAll        (0U-1)                                        // signal mask for all signals
+#define SIGSET(signo) ((((signo)-1) < SIG_LIMIT) ? 1U<<((signo)-1) : 0U) // signal mask from the given signal number
+#define sigAll        (0U-1)                                             // signal mask for all signals
+
 /* -------------------------------------------------------------------------- */
 // define and initialize the protected signal (sig)
 #define OS_SIG(sig)                     sig_t sig[] = { 0 }
